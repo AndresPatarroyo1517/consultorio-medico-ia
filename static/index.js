d76f5fb77c11401e1d9a73f4e3523639376e8f1d
Andrés Patarroyo <159494183+AndresPatarroyo1517@users.noreply.github.com>
2024-05-21 22:44:19 -0500

    Primeros pasos y HTML definido

---
+++ static/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    var promptsAnteriores = [];
+    $("#buscar").on("click", () => {
+        var promptActual = $("#inputPregunta").val();
+        promptsAnteriores.push(promptActual);
+        var cuerpo = {
+            "model": "llama3:8b",
+            "prompt": promptActual,
+            "stream": true
+        }
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:11434/api/generate",
+            data: JSON.stringify(cuerpo),
+            xhrFields: {
+                onprogress: function(e) {
+                    var response = e.currentTarget.response;
+                    var lines = response.split('\n');
+                    var textoAnterior = $("#textaRespuesta").text()
+                    lines.forEach(function(line) {
+                        if (line.trim() !== '') {
+                            var responseObject = JSON.parse(line);
+                            console.log(responseObject);
+                            $("#textaRespuesta").text(`${textoAnterior}${responseObject.response}`);
+                        }
+                    });
+                }
+            },
+        }).done(function(data){
+            console.log(data)
+        });
+    });
+});
+
